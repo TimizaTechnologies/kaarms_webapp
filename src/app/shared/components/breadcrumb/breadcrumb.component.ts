7b27f3dcdb3d2619f839c9635c2001a62cb790ff
Kelvince01 <themikrochip1@gmail.com>
2023-01-05 16:44:08 +0300

    breadcrumbs, permissions and more

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Breadcrumb } from '@shared/models/breadcrumb.model';
+import { BreadcrumbService } from '@shared/services/breadcrumb.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.css'],
+})
+export class BreadcrumbComponent {
+  breadcrumbs$: Observable<Breadcrumb[]>;
+
+  constructor(private readonly breadcrumbService: BreadcrumbService) {
+    this.breadcrumbs$ = this.breadcrumbService.breadcrumbs$;
+  }
+}
